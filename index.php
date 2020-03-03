c369fbe903280b515daed44f8e1d1fddf61381fa
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-03 19:18:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $load = (isset($_GET['page'])&& $_GET['page'] !='')? $_GET['page'] : '';
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit:800i|Oswald|Teko:600&display=swap">
 	 <div class="w3-bar-block w3-dark-grey w3-center w3-margin w3-round">
 			<li class="w3-bar-block w3-large w3-button w3-green w3-hover-white  w3-text-decoration-none; text-align: center;"><a href="index.php?page=list">Products</a></li>
-			<li class="w3-bar-block w3-large w3-button w3-green w3-hover-white  w3-text-decoration-none; text-align: center;"><a href="index.php?page=show_product&id=<?php echo $result['id'];?>"> <?php echo $result['name']; ?>Description</a></li>
+			<li class="w3-bar-block w3-large w3-button w3-green w3-hover-white  w3-text-decoration-none; text-align: center;"><a href="index.php?page=show_product&id=<?php echo $result['id'];?>"> <?php echo $result['name']; ?>">Description</a></li>
 		 	<li class="w3-bar-block w3-large w3-button w3-green w3-hover-white  w3-text-decoration-none; text-align: center;"><a href="index.php?page=create">Create</a></li>
 	</div>
 <?php

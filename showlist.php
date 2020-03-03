0ed07444ae1c14bef91d36210ebcbceed5e6afc1
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-03 19:55:58 +0800

    Update showlist.php

--- showlist.php
+++ showlist.php
@@ -16,12 +16,15 @@
  
 ?>
 <html>
+<head>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+</head>
 <br/>
 <div class="w3-container w3-margin w3-round">
 	</br></br>
 	<form action="index.php?page=list" method="POST">
-	Search:<input type="text" name="search" placeholder="Search Product Name">
-		<input type="submit" name="submit"><i class="fa fa-search"> </i>
+	Search:<input class="text" name="search" placeholder="Product Name">
+		<button class="w3-button w3-blue-grey" type="submit" name="submit"><i class='fas fa-paper-plane'></i></button>
 	</form>
 	<hr/>
   <table class="w3-table-all w3-round w3-hoverable">

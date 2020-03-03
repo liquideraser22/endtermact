96d03b7b23b47fb889e260313bb244518379241b
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-03 19:45:07 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -21,7 +21,7 @@
 <br/>
 <div class="w3-container">
 <form action="pro_update.php?id=<?php echo $id ?>" method="POST">
-	<form class="w3-container w3-green"
+	<form class="w3-container w3-green">
 	Name:<br/><input class="w3-input w3-border w3-round-large" type="text" name="name" value="<?php echo $result['name'];?>"/><br/><br/>
 	Description:<br/><textarea class="w3-input w3-border w3-round-large" name="description"><?php echo $result['description']; ?></textarea><br/><br/>
 	Price:<br/><input class="w3-input w3-border w3-round-large" type="number" name="price" value="<?php echo $result['price']; ?>"/><br/><br/>

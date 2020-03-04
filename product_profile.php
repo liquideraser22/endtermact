8e71c2861c0b848077f5eefeda20675de63524c2
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-04 22:22:10 +0800

    Update product_profile.php

--- product_profile.php
+++ product_profile.php
@@ -14,12 +14,18 @@
 		<header class="w3-container w3-dark-grey w3-round-large">
 			<h1><b><?php echo $result['name']; ?></b></h1>
 		</header>
-	<table class="w3-table-all w3-round w3-hoverable">
-		 <tr class="w3-dark-grey">
-		<th class="w3-center w3-animate-top"><?php echo $result['description']; ?> </th>
-		<th class="w3-center w3-animate-top"><?php echo $result['price']; ?> </th>
-		<th class="w3-center w3-animate-top"><?php echo $result['category_name'];?> </th>
-	  </tr>
+		<table class="w3-table-all w3-round w3-hoverable">
+		<tr class="w3-dark-grey">
+		<th class="w3-center w3-animate-top">Price</th>
+     	       	<th class="w3-center w3-animate-top">Description</th>
+      		<th class="w3-center w3-animate-top">Category</th>
+		</tr>	
+		
+		<tr>
+		<td class="w3-center"><?php echo $result['description']; ?></td>
+		<td class="w3-center"><?php echo $result['price']; ?> </td>
+		<td class="w3-center"><?php echo $result['category_name'];?> </td>
+	 	 </tr>
 		<footer class="w3-container w3-dark-grey w3-padding-small w3-round-large">
 			<a class="w3-button w3-round-large w3-right w3-teal w3-margin" href="form_update.php?id=<?php echo $id ?>">Edit</a>
 			<a class="w3-button w3-round-large w3-right w3-red w3-margin" href="pro_delete.php?id=<?php echo $id ?>">Delete</a>

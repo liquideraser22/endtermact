2b6e57b18b54ab94d09365430aa3b3d70512a915
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-04 21:00:02 +0800

    Update showlist.php

--- showlist.php
+++ showlist.php
@@ -25,7 +25,7 @@
 	<form action="index.php?page=list" method="POST">
 	<div class="w3-container w3-show">
 	<input class="w3-border w3-round-large" type="text"name="search" placeholder="Search product name"> 
-		<button class="w3-button w3-black w3-animate-zoom" type="submit" name="submit"><i class='fa fa-search'></i></button>
+		<button class="w3-button w3-white w3-animate-zoom" type="submit" name="submit"><i class='fa fa-search'></i></button>
 		</div>	
 	</form>
 	<hr/>
